18d91bed79b8535631db76ecf877766160f3a663
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-04 10:47:27 -0500

    Day 4 part two

--- 2020/04_passports.rb
+++ 2020/04_passports.rb
@@ -1,29 +1,45 @@
 # frozen_string_literal: true
 
 class Passport
-  REQUIRED = [
-    'byr', # (Birth Year)
-    'iyr', # (Issue Year)
-    'eyr', # (Expiration Year)
-    'hgt', # (Height)
-    'hcl', # (Hair Color)
-    'ecl', # (Eye Color)
-    'pid', # (Passport ID)
-  # 'cid', # (Country ID)
-  ]
+
+  HEIGHT_RANGES = {
+    'cm' => [150, 193],
+    'in' => [59, 76],
+  }
+
+  REQUIRED = {
+    'byr' => -> (field) { field.to_i.between?(1920, 2002) }, # (Birth Year)
+    'iyr' => -> (field) { field.to_i.between?(2010, 2020) }, # (Issue Year)
+    'eyr' => -> (field) { field.to_i.between?(2020, 2030) }, # (Expiration Year)
+    'hgt' => -> (field) { valid_height?(field) }, # (Height)
+    'hcl' => -> (field) { field.match?(/^#[0-9a-f]{6}$/) }, # (Hair Color)
+    'ecl' => -> (field) { %w(amb blu brn gry grn hzl oth).include?(field) }, # (Eye Color)
+    'pid' => -> (field) { field.match?(/^[0-9]{9}$/) }, # (Passport ID)
+    # 'cid', # (Country ID)
+  }
 
   def initialize(fields)
     @fields = parse(fields)
   end
 
   def valid?
-    (REQUIRED - @fields.keys).empty?
+    return false unless (REQUIRED.keys - @fields.keys).empty?
+    REQUIRED.each do |key, rule|
+      return false unless rule.call(@fields[key])
+    end
+    true
   end
 
   private
 
   def parse(data)
-    Hash[data.map{|field| field.split(':')}]
+    Hash[data.map { |field| field.split(':') }]
+  end
+
+  def self.valid_height?(field)
+    unit = field[-2..-1]
+    return false unless HEIGHT_RANGES.keys.include?(unit)
+    field.to_i.between?(HEIGHT_RANGES[unit][0], HEIGHT_RANGES[unit][1])
   end
 end
 
@@ -32,7 +48,7 @@ class Passports
     @passports = parse(input)
   end
 
-  def part_one
+  def part_two
     @passports.count(&:valid?)
   end
 
@@ -41,7 +57,7 @@ class Passports
   def parse(input)
     input
       .split("\n\n")
-      .map{|passport| Passport.new(passport.split(/\s+/))}
+      .map { |passport| Passport.new(passport.split(/\s+/)) }
   end
 end
 
@@ -49,5 +65,5 @@ if __FILE__ == $0
   filename = "04_input.txt"
   input = File.read(filename)
   passports = Passports.new(input)
-  puts passports.part_one
+  puts passports.part_two
 end
