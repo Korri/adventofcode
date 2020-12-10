c86a713c776b73b675933febea60b235d04cab72
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-10 14:55:42 -0500

    Day 10 part 2

--- 2020/10_jolt_chainer.rb
+++ 2020/10_jolt_chainer.rb
@@ -9,6 +9,8 @@ class JoltChainer
       2 => 0,
       3 => 1,
     }
+    @chains = []
+    @chain_count_cache = {}
   end
 
   def chain
@@ -21,13 +23,26 @@ class JoltChainer
 
     chain
   end
+
+  def chain_counts(jolts)
+    @chain_count_cache[jolts] ||= begin
+      return 1 if jolts == @adapters.last
+      next_adapters = @adapters.select{|adapter| adapter.between?(jolts + 1, jolts + 3)}
+      next_adapters.sum {|next_adapter| chain_counts(next_adapter)}
+    end
+  end
 end
 
 if __FILE__ == $0
   filename = "10_input.txt"
   input = File.read(filename)
-  console = JoltChainer.new(input.split("\n").map(&:to_i))
-  jumps = console.chain
-  p jumps
-  p jumps[1] * jumps[3]
+  # chainer = JoltChainer.new(input.split("\n").map(&:to_i))
+  # jumps = chainer.chain
+  # # p jumps
+  # p jumps[1] * jumps[3]
+
+  chainer = JoltChainer.new(input.split("\n").map(&:to_i))
+  count = chainer.chain_counts(0)
+  # chains.each{|chain| p chain }
+  p count
 end
