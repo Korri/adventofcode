dcdd9641ef69d65b517f36ceb1f8118d0c219103
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-14 10:47:22 -0500

    Day 14 part two

--- 2020/14_docking.rb
+++ 2020/14_docking.rb
@@ -16,15 +16,45 @@ class Docking
     memory
   end
 
+  def run_part_two
+    memory = {}
+    @instuctions.each do |operation, slot, value|
+      puts "#{operation}[#{slot}] = #{value}"
+      if operation == 'mask'
+        set_mask(value)
+      else
+        apply_mask_v2(slot.to_i).each do |masked_slot|
+          puts "masked_mem[#{masked_slot}] = #{value}"
+          memory[masked_slot] = value.to_i
+        end
+      end
+    end
+    memory.values
+  end
+
   private
 
   def set_mask(mask)
+    @mask = mask
     @and_mask = mask.gsub('X', '1').to_i(2)
     @or_mask = mask.gsub('X', '0').to_i(2)
   end
 
   def apply_mask(value)
     value & @and_mask | @or_mask
+  end
+
+  def apply_mask_v2(address)
+    or_address = address | @or_mask
+    permutations_count = @mask.count('X')
+    ['0','1'].repeated_permutation(permutations_count).map do |bits|
+      permutation_address = or_address.to_s(2).rjust(36, '0')
+      @mask.split(//).each_with_index do |char, index|
+        next unless char == 'X'
+        permutation_address[index] = bits.pop
+      end
+      permutation_address.to_i(2)
+    end
   end
 end
 
@@ -34,5 +64,5 @@ if __FILE__ == $0
   instructions = input.scan(/(mem|mask)(?:\[(\d+)\])? = (.+)/)
 
   docking = Docking.new(instructions)
-  p docking.run_part_one.sum { |val| val || 0 }
+  p docking.run_part_two.sum { |val| val || 0 }
 end
