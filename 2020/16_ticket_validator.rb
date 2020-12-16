ecb35fbe650859c5017748dc2d6ae5ef455cd088
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-16 11:33:18 -0500

    Day 16 part 2

--- 2020/16_ticket_validator.rb
+++ 2020/16_ticket_validator.rb
@@ -16,9 +16,20 @@ class TicketValidator
     @rules = rules.map { |line| ValidationRule.new(line) }
   end
 
-  def valid?(ticket)
-    fields = ticket.split(',').map(&:to_i)
-    fields.all? { |field| valid_value?(field) }
+  def fields_order(tickets)
+    tickets = tickets.map{|string| string.split(',').map(&:to_i)}
+    tickets = tickets.select{|ticket| valid_ticket?(ticket) }
+
+    rule_names = @rules.map(&:name)
+    fields_validity = tickets[0].map{rule_names.dup}
+
+    tickets.each do |ticket|
+      ticket.each_with_index do |value, index|
+        fields_validity[index] = fields_validity[index] & valid_rule_names(value)
+      end
+    end
+
+    reduce_rules(fields_validity)
   end
 
   def ticket_error_rate(ticket)
@@ -29,6 +40,26 @@ class TicketValidator
 
   private
 
+  def reduce_rules(validity)
+    while validity.map{|field| field.count}.max > 1
+      settled_rules = validity.select{|rules| rules.count == 1}.map(&:first)
+      validity = validity.map do |rules|
+        next rules if rules.count == 1
+        rules - settled_rules
+      end
+    end
+
+    validity.map(&:first)
+  end
+
+  def valid_ticket?(fields)
+    fields.all? { |field| valid_value?(field) }
+  end
+
+  def valid_rule_names(value)
+    @rules.select { |rule| rule.valid?(value) }.map(&:name)
+  end
+
   def valid_value?(value)
     @rules.any? { |rule| rule.valid?(value) }
   end
@@ -38,11 +69,16 @@ end
 if __FILE__ == $0
   filename = "16_input.txt"
   rules, ticket, nearby = File.read(filename).split("\n\n")
-  ticket = ticket.split("\n")[1]
+  ticket = ticket.split("\n")[1].split(',').map(&:to_i)
   nearby = nearby.split("\n")[1..]
 
   validator = TicketValidator.new(rules.split("\n"))
   error_rate = nearby.sum { |ticket| validator.ticket_error_rate(ticket).sum }
 
   puts "Error rate: #{error_rate}"
+  field_names = validator.fields_order(nearby)
+  values =field_names.each_with_index.map do |rule_name, index|
+    rule_name[0..8] == 'departure' ? ticket[index] : 1
+  end
+  puts "Ticket Factor: #{values.reduce(1, &:*)}"
 end
