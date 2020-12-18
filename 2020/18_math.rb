3f9fa52dc92dbfab0fd1fb66f5dd6916c0538aff
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-18 16:04:32 -0500

    Day 18 part two

--- 2020/18_math.rb
+++ 2020/18_math.rb
@@ -1,6 +1,6 @@
 module Math
   PARENTHESES = /\(([^)(]+)\)/
-  OPERATION = /(\d+)([-+*\/])(\d+)/
+  OPERATION = [/(\d+)([-+])(\d+)/, /(\d+)([*\/])(\d+)/]
 
   def self.eval(expr)
     expr = expr.dup.gsub(/\s+/, '')
@@ -8,11 +8,12 @@ module Math
     while expr.match(PARENTHESES) do
       expr = expr.sub(PARENTHESES) { self.eval($1) }
     end
-
-    while expr.match(OPERATION) do
-      expr = expr.sub(OPERATION) do
-        a, op, b = $1.to_i, $2, $3.to_i
-        a.send(op, b)
+    OPERATION.each do |operation|
+      while expr.match(operation) do
+        expr = expr.sub(operation) do
+          a, op, b = $1.to_i, $2, $3.to_i
+          a.send(op, b)
+        end
       end
     end
     expr.to_i
@@ -23,5 +24,5 @@ if __FILE__ == $0
   filename = "18_input.txt"
   input = File.read(filename)
 
-  puts input.split("\n").sum{|operation| Math.eval(operation) }
+  puts input.split("\n").sum { |formula| Math.eval(formula) }
 end
