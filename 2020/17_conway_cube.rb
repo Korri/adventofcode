a61c423edf5a275f7fff407a4393709c1b66771b
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-18 15:29:30 -0500

    Day 17 part two

--- 2020/17_conway_cube.rb
+++ 2020/17_conway_cube.rb
@@ -7,24 +7,17 @@ class PocketDimension
 
   def tick(times)
     initialize_map(times)
-    # puts "Before any cycles:"
-    # puts self
-    times.times do |cycle|
+    times.times do
       new_map = @map.dup
 
       iterate do |pos|
         cube = @map[pos]
-        active_neighbours = neighbours(pos).select {|neighbour| @map[neighbour] == '#'}
-        if active_neighbours.count > 1
-          #puts "#{pos.inspect} has active neighbours: #{active_neighbours.inspect}"
-        end
-        active_count = neighbours(pos).count {|neighbour| @map[neighbour] == '#'}
+        active_count = neighbours(pos).count { |neighbour| @map[neighbour] == '#' }
         new_map[pos] = '#' if cube == '.' && active_count == 3
-        new_map[pos] = '.' if cube == '#' && !active_count.between?(2,3)
+        new_map[pos] = '.' if cube == '#' && !active_count.between?(2, 3)
       end
       @map = new_map
-      # puts "After #{cycle+1} cycles:"
-      # puts self
+      puts count_active
     end
   end
 
@@ -36,39 +29,23 @@ class PocketDimension
     count
   end
 
-  def to_s
-    z = nil
-    y = nil
-    result = ''
-    iterate do |pos|
-      if z != pos[2]
-        result += "\n\nz=#{pos[2].to_s}"
-        z = pos[2]
-      end
-      if y != pos[1]
-        result += "\n"
-        y = pos[1]
-      end
-      result += @map[pos]
-    end
-    result
-  end
-
   private
 
   def iterate
-    @height.times do |z|
-      @depth.times do |y|
-        @width.times do |x|
-          yield [x, y, z]
+    @height.times do |w|
+      @height.times do |z|
+        @depth.times do |y|
+          @width.times do |x|
+            yield [x, y, z, w]
+          end
         end
       end
     end
   end
 
   def neighbours(pos)
-    values = pos.map{|value| (value-1..value+1).to_a}
-    values[0].product(values[1], values[2]) - [pos]
+    values = pos.map { |value| (value - 1..value + 1).to_a }
+    values[0].product(values[1], values[2], values[3]) - [pos]
   end
 
   def initialize_map(times)
@@ -76,9 +53,10 @@ class PocketDimension
     @width = @first_layer[0].count + times * 2
     @depth = @first_layer.count + times * 2
     @height = 1 + times * 2
+
     iterate do |pos|
       @map[pos] = '.'
-      if pos[2] === times && pos[0] >= times && pos[1] >= times
+      if pos[2] === times && pos[3] === times && pos[0] >= times && pos[1] >= times
         first_layer_x = pos[0] - times
         first_layer_y = pos[1] - times
         row = @first_layer[first_layer_y]
@@ -94,5 +72,4 @@ if __FILE__ == $0
 
   map = PocketDimension.new(input)
   map.tick(6)
-  puts map.count_active
 end
