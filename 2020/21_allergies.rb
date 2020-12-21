07d4ca878f48ac78c149a7d3d24bf535214143b2
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-21 12:32:48 -0500

    Day 21 part two

--- 2020/21_allergies.rb
+++ 2020/21_allergies.rb
@@ -18,6 +18,19 @@ class Allergies
     non_allergens = @ingredients.uniq - @allergens.values.flatten.uniq
     non_allergens.sum{|ingredient| @ingredients.count(ingredient) }
   end
+
+  def figure_out_allergens
+    data = @allergens.dup
+    while data.values.map{|ingredient| ingredient.count}.max > 1
+      found_ingredients = data.values.select{|ingredient| ingredient.count == 1}.map(&:first)
+      data = data.map do |allergen, ingredients|
+        next [allergen, ingredients] if ingredients.count == 1
+        [allergen, ingredients - found_ingredients]
+      end.to_h
+    end
+
+    data.map{|k, v| [k, v[0]]}.to_h
+  end
 end
 
 if __FILE__ == $0
@@ -25,4 +38,7 @@ if __FILE__ == $0
 
   allergies = Allergies.new(input.split("\n"))
   p allergies.non_allergen_count
+  allergens = allergies.figure_out_allergens
+
+  p allergens.sort.to_h.values.join(',')
 end
