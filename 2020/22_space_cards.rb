b781426aacea980db65b0776f7d8868d4344979f
Hugo Vacher <hugo.vacher@shopify.com>
2020-12-23 21:56:08 -0500

    Day 22 part two

--- 2020/22_space_cards.rb
+++ 2020/22_space_cards.rb
@@ -1,24 +1,48 @@
 class SpaceCards
   def initialize(decks)
     @decks = decks
+    @rounds_history = []
   end
 
   def round
     cards = @decks.map(&:shift)
-    non_nil_cards = cards.select.to_a
-    winner = cards.index(non_nil_cards.max)
-    @decks[winner] += non_nil_cards.sort.reverse
+    if cards.each_with_index.all?{|card, index| card <= @decks[index].count}
+      winner = play_sub_game(cards)
+    else
+      winner = cards.index(cards.max)
+    end
+    winner_s_card = cards[winner]
+    @decks[winner] += [winner_s_card, *(cards - [winner_s_card])]
   end
 
   def play!
     while @decks.select{|deck| deck.count > 0}.count > 1
+      return 0 if already_played?
       round
     end
-    @decks.find(&:any?)
+    winner = @decks.find(&:any?)
+    @decks.index(winner)
   end
 
-  def self.score(winner)
-    winner.reverse.each_with_index.map{|card, index| card * (index + 1)}.sum
+  def score(winner)
+    deck = @decks[winner]
+    deck.reverse.each_with_index.map{|card, index| card * (index + 1)}.sum
+  end
+
+  private
+
+  def already_played?
+    if @rounds_history.include?(@decks)
+      return true
+    end
+    @rounds_history << @decks.map(&:dup)
+    false
+  end
+
+  def play_sub_game(cards)
+    new_decks = @decks.each_with_index.map { |deck, index| deck[0...cards[index]] }
+    sub_game = SpaceCards.new(new_decks)
+    sub_game.play!
   end
 end
 
@@ -29,5 +53,5 @@ if __FILE__ == $0
   game = SpaceCards.new(players_cards)
 
   winner = game.play!
-  p SpaceCards.score(winner)
+  p game.score(winner)
 end
