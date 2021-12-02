85789434c3be6c941b8a7d9be9f9c6289ffd54f6
Hugo Vacher <korrigan33@gmail.com>
2021-12-02 09:12:51 -0500

    Day 2 part 1

---
+++ 2021/02_pilot.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Pilot
+  DIRECTIONS = {
+    'forward' => [1, 0],
+    'down' => [0, 1],
+    'up' => [0, -1],
+  }
+
+  def initialize
+    @x = 0
+    @depth = 0
+  end
+
+  def navigate(moves)
+    moves.each do |instruction|
+      move(instruction)
+    end
+  end
+
+  def move(instruction)
+    direction, amount = instruction.split(' ')
+    amount = amount.to_i
+    x_change, depth_change = DIRECTIONS[direction]
+
+    @x += x_change * amount
+    @depth += depth_change * amount
+  end
+
+  def print
+    puts "Position: #{@x}, #{@depth}: #{@x * @depth}"
+  end
+end
+
+if __FILE__ == $0
+  filename = "02_input.txt"
+  input = File.readlines(filename).map(&:strip)
+
+  pilot = Pilot.new
+  pilot.navigate(input)
+  pilot.print
+end
