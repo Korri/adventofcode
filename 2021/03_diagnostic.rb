e1bddebc3b431584d3f6f14bd6b3bd9b335418b5
Hugo Vacher <>
2021-12-03 11:08:42 -0500

    Day 3 part 2

--- 2021/03_diagnostic.rb
+++ 2021/03_diagnostic.rb
@@ -5,12 +5,20 @@ class Diagnostic
     @input = input
   end
 
+  def transposed
+    @input.map{|item| item.split("") }.transpose
+  end
+
   def bits
     @bits ||= begin
-      @input.map{|item| item.split("") }.transpose.map do |column|
+      transposed.map do |column|
         column.max_by {|i| column.count(i)}
       end
     end
+  end
+
+  def flipped_ints
+    @flipped_ints ||= @input.map{|i| i.reverse.to_i(2)}
   end
 
   def gamma
@@ -19,6 +27,30 @@ class Diagnostic
 
   def epsilon
     bits.map{|i| i == "1" ? 0 : 1}.join("").to_i(2)
+  end
+
+  def oxygen
+    gaz(:max_by)
+  end
+
+  def co2
+    gaz(:min_by)
+  end
+
+  def gaz(method)
+    bits = ""
+    matches = @input
+    while matches.length > 1
+      columns = matches.map{|item| item.split("") }.transpose
+      column = columns[bits.length]
+      bits += column.send(method){|i| column.count(i) + i.to_f/2}
+      matches = find_matches(matches, bits)
+    end
+    matches[0].to_i(2)
+  end
+
+  def find_matches(matches, bits)
+    matches.select { |i| i.start_with?(bits) }
   end
 end
 
@@ -30,4 +62,9 @@ if __FILE__ == $0
   puts diag.gamma
   puts diag.epsilon
   puts diag.gamma * diag.epsilon
+
+
+  puts diag.oxygen
+  puts diag.co2
+  puts diag.oxygen * diag.co2
 end
