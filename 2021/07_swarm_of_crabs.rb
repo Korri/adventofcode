d54691f7338dec2f2f738c56725c8c7ae038a8e3
Hugo Vacher <>
2021-12-12 12:57:39 +0100

    Day 7 part 1

---
+++ 2021/07_swarm_of_crabs.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+class Crab
+  def initialize(position)
+    @position = position
+  end
+
+  def fuel_to(position)
+    (@position - position).abs
+  end
+end
+
+class Swarm
+  def initialize(positions)
+    @positions = positions.sort
+    @crabs = @positions.map { |position| Crab.new(position) }
+  end
+  def best_position
+    (@positions.first..@positions.max).map do |target|
+      @crabs.map { |crab| crab.fuel_to(target) }.sum
+    end.min
+  end
+end
+
+if __FILE__ == $0
+  filename = "07_input.txt"
+  input = File.readlines(filename).first.split(',').map(&:to_i)
+
+  puts Swarm.new(input).best_position
+end
